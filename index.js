64a239a554f2de02dc53a5112179a857d98c77f8
unknown <wixproyecto2@gmail.com>
2022-05-18 15:59:53 -0600

    final 2020

--- index.js
+++
@@ -1,9 +0,0 @@
-import { app } from "./src/app.js";
-import { createConnection } from "./src/database.js";
-
-createConnection();
-app.listen(app.get("port"), ()=>{
-  
-});
-
-
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import { app } from "../app.js";
+import { createConnection } from "../database.js";
+
+createConnection();
+app.listen(app.get("port"), ()=>{
+  
+});
+
+
